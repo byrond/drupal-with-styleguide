42cf3f261bca61b871949422d86abdae483a45e5
Byron Duvall <duvall@palantir.net>
2022-12-23 11:50:09 -0500

    use 97-pa11y branch of palantir-lab to support M1 Macs

--- styleguide/gulp-tasks/accessibility.js
+++ styleguide/gulp-tasks/accessibility.js
@@ -2,35 +2,24 @@
 
 'use strict';
 
-// Include gulp
-const { src } = require('gulp');
-
 // Include Our Plugins
 const log = require('fancylog');
-const accessibility = require('gulp-accessibility');
+const pa11y = require('pa11y');
+const fs = require("fs");
 
-// Export our tasks.
-module.exports = {
-  // Lint Sass based on .stylelintrc.yml config.
-  accessibilityAudit: function () {
-    log.info('Auditing Accessibility');
-    return src([
-      "public/patterns/**/*.html",
-      "!public/patterns/**/*.markup-only.html",
-      "!public/patterns/**/index.html",
-      "!public/patterns/base/*",
-      "!public/patterns/elements/*",
-      "!public/patterns/components/*",
-      "!public/patterns/templates/*",
-      "!public/patterns/pages/*",
-      "!public/patterns/elements-forms/*.html",
-      "!public/patterns/elements-forms-*/*.html",
-      "!public/patterns/base-colors/*.html"
-    ])
-    .pipe(accessibility({
-      force: true,
-      accessibilityLevel: 'WCAG2AA',
-      reportLevels: { notice: false, warning: false, error: true },
+async function runPa11y(src) {
+  try {
+    const results = await pa11y(src,{
+      standard: 'WCAG2AA',
+      level: "error",
+      log: {
+        debug: console.log,
+        error: console.error,
+        info: console.info
+      },
+      runners: [
+        'htmlcs'
+      ],
       ignore: [
         // This rule:
         // The html element should have a lang or xml:lang attribute which describes the language of the document.
@@ -38,7 +27,45 @@ module.exports = {
         'WCAG2A.Principle3.Guideline3_1.3_1_1.H57.2',
         'WCAG2AA.Principle3.Guideline3_1.3_1_1.H57.2'
       ]
-    }))
-    .on('error', console.log)
+    }).then((results) => {
+      console.log(results);
+    });
+    // Do something with the results
+  } catch (error) {
+    // Handle the error
+    console.log(error);
+  }
+  return src;
+}
+
+const walk = function(dir) {
+  var results = [];
+  var list = fs.readdirSync(dir);
+  list.forEach(function(file) {
+    file = dir + '/' + file;
+    var stat = fs.statSync(file);
+    if (stat && stat.isDirectory()) {
+      /* Recurse into a subdirectory */
+      results = results.concat(walk(file));
+    } else {
+      /* Is a file */
+      const name = file.split('/').pop();
+      if (name === "index.html") {
+        results.push(file);
+      }
+    }
+  });
+  return results;
+}
+
+// Export our tasks.
+module.exports = {
+  // Lint Sass based on .stylelintrc.yml config.
+  accessibilityAudit: async function () {
+    log.info('Auditing Accessibility');
+    const files = walk("public/patterns");
+    files.forEach((file) => {
+      runPa11y(file);
+    });
   }
 };
